4e2038802da661ad7ab8fef68dadeec05feb990e
柾本 耕太郎 <kotaromasamoto@creation-2.local>
2018-04-07 11:09:40 +0900

    add test plugin(Android/Java)

---
+++ plugin/android/java/jar/android/plugin/src/main/java/com/test/TestPlugin.java
@@ -0,0 +1,15 @@
+//======================================================================
+// Project Name    : java
+//
+// Copyright © 2018 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+//======================================================================
+package com.test;
+public class TestPlugin {
+    public int execute() {
+        return 1 + 1;
+    }
+}
