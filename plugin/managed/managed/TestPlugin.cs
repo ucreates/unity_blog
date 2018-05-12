1915ec3c310c2cc17291fe8b4d697f406036a5a8
柾本 耕太郎 <kotaromasamoto@creation-2.local>
2018-05-12 09:59:01 +0900

    add test plugin(iOS/Android/Managed)

---
+++ plugin/managed/managed/TestPlugin.cs
@@ -0,0 +1,18 @@
+﻿// ======================================================================
+// Project Name    : managed
+//
+// Copyright © 2018 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+//======================================================================
+using UnityEngine;
+namespace UnityManagedPlugin {
+public class TestPlugin {
+    public int Execute() {
+        Debug.Log(string.Format("{0}.{1}", this.GetType().Name, new System.Diagnostics.StackFrame(0, true).GetMethod().Name));
+        return 1 + 1;
+    }
+}
+}
---
+++ plugin/managed/unity/Assets/Plugins/Unity/TestPlugin.cs
@@ -0,0 +1,23 @@
+﻿//======================================================================
+// Project Name    : managed
+//
+// Copyright © 2018 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+//======================================================================
+using UnityEngine;
+namespace UnityPlugin {
+public class TestPlugin {
+    public int Execute() {
+        Debug.Log(string.Format("{0}.{1}", this.GetType().Name, new System.Diagnostics.StackFrame(0, true).GetMethod().Name));
+        int ret = 0;
+        if (RuntimePlatform.IPhonePlayer == Application.platform || RuntimePlatform.Android == Application.platform) {
+            UnityManagedPlugin.TestPlugin plugin = new UnityManagedPlugin.TestPlugin();
+            ret = plugin.Execute();
+        }
+        return ret;
+    }
+}
+}
